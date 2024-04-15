0bbb3c5c23d0ad01fb53c8ce400a12510add92ea
Emanuel Acosta Gonzalez (emanvidmaker) <emanvidmaker@gmail.com>
2024-04-15 14:40:08 -0400

    init

---
+++ addons/Work-Clock/workclock.gd
@@ -0,0 +1,23 @@
+@tool
+extends EditorPlugin
+const CLOCK = preload("res://addons/workclock/clock.tscn")
+var instance:Node
+const dock = EditorPlugin.CONTAINER_INSPECTOR_BOTTOM
+func _enter_tree() -> void:
+	instance = CLOCK.instantiate()
+	add_control_to_container(
+		dock,
+		instance
+		)
+	# Initialization of the plugin goes here.
+	pass
+
+
+func _exit_tree() -> void:
+	remove_control_from_container(
+		dock,
+		instance
+		)
+	instance.queue_free()
+	# Clean-up of the plugin goes here.
+	pass
